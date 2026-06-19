c7d54e69329e320f8561dcadcb3a70f5957680e9
lausoar <lausoar@163.com>
2026-06-19 23:12:19 +0800

    测试提交2 [codesave: 20260619-231142,20260619-231055]

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -4,6 +4,8 @@ const Layout = () => {
       <h1>Layout</h1>
       <p>This is the layout page.</p>
       <p>Here you can find various layout options and examples.</p>
+      <p>Feel free to explore and customize your layout as needed.</p>
+      <p>Remember to save your changes and test your layout on different devices for responsiveness.</p>
     </div>
   );
 };
