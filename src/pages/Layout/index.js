da99343e99982f7cbcd0e0aee1e7b101e8c1d41f
lausoar <lausoar@163.com>
2026-06-19 22:48:53 +0800

    测试提交 [codesave: 20260619-224816,20260619-224633,20260619-224535]

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -2,6 +2,8 @@ const Layout = () => {
   return (
     <div>
       <h1>Layout</h1>
+      <p>This is the layout page.</p>
+      <p>Here you can find various layout options and examples.</p>
     </div>
   );
 };
