26e9b125bb2f8e450c4333eb45362e04862ebae7
lausoar <lausoar@163.com>
2026-06-20 18:05:54 +0800

    删除测试 [codesave: 20260620-180524,20260620-180441]

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -2,12 +2,6 @@ const Layout = () => {
   return (
     <div>
       <h1>Layout</h1>
-      <p>This is the layout page.</p>
-      <p>Here you can find various layout options and examples.</p>
-      <p>Feel free to explore and customize your layout as needed.</p>
-      <p>Remember to save your changes and test your layout on different devices for responsiveness.</p>
-      <h2>Layout Examples</h2>
-      <p>Here are some popular layout techniques you can use:</p>
       <ul>
         <li>Grid Layout</li>
         <li>Flexbox Layout</li>
@@ -16,7 +10,6 @@ const Layout = () => {
       </ul>
 
       <P>For more information on layout techniques, check out the documentation and tutorials available online.</P>
-      <p>Happy coding!</p>
       <div>
         <p>Note: This layout page is a work in progress and may be updated with new features and examples in the future.</p>
       </div>
